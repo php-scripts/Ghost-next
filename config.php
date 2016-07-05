f71c7e373c46fa5b2b450f3b4c97baab5812605c
masrourmouad <masrourmouad@yahoo.fr>
2016-07-05 21:43:11 +0000

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
     //All Parameters
     $param = array();
-    $param["site_link"] = "https://code.google.com/p/ghost/";
+    $param["site_link"] = "https://github.com/masrourmouad/Ghost-next";
     $param["wsite_url"] = "http://ghost.atspace.eu/";
     $param["site_shrt"] = "Ghost";
-    $param["chat_purg"] = "armada";
+    $param["chat_purg"] = "evaniceguy";
     $param["feeedback"] = "on";//on or off / enable or disable
     $param["slog_stat"] = "on";//on or off / enable or disable
     
